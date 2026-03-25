3798267858cef736d90e3a1678ef3f4af840b0c4
Joonsuh Choi <kwyxl33872@gmail.com>
2026-03-24 21:55:37 -0400

    start

--- electron-preload.js
+++ electron-preload.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // Since contextIsolation is false, we can expose directly to window
 const { ipcRenderer } = require('electron');
 
@@ -8,14 +7,3 @@ window.electronShell = {
     chromeVersion: process.versions.chrome,
     toggleDevTools: () => ipcRenderer.invoke('toggle-devtools')
 };
-=======
-// Since contextIsolation is false, we can expose directly to window
-const { ipcRenderer } = require('electron');
-
-window.electronShell = {
-    isElectron: true,
-    electronVersion: process.versions.electron,
-    chromeVersion: process.versions.chrome,
-    toggleDevTools: () => ipcRenderer.invoke('toggle-devtools')
-};
->>>>>>> ea626d7be2661ff009d4f1e7032eb2b0d1170146
